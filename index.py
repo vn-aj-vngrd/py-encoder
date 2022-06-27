7960ff6eff40f40fd4895f5b0c1106496aed01f4
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-06-28 06:34:52 +0800

    init

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
 
+# Get the location of the data
 path = "./test.xlsx"
 
+# Read the data
 df_dict = pd.read_excel(path, sheet_name=None)
+
+# Get sheet names
 df_sheetNames = pd.ExcelFile(path)
 
+# Locate the data
 # newData = data.iloc[0,2]
 
-# print(newData)
-
-print(df_dict.sheet_names)
