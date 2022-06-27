31bf65e9a0999a91e7ad258a4ac1e84b4d0ef1ce
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-06-28 06:38:00 +0800

    init

--- index.py
+++ index.py
@@ -7,9 +7,13 @@ path = "./test.xlsx"
 # Read the data
 df_dict = pd.read_excel(path, sheet_name=None)
 
+# Combine data from all worksheets as single DataFrame
+df_all = pd.concat(df_dict.values(), ignore_index=True)
+
 # Get sheet names
 df_sheetNames = pd.ExcelFile(path)
 
 # Locate the data
-# newData = data.iloc[0,2]
+machinery = df_all.iloc[0,2]
 
+print(machinery)
